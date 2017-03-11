1017b457ab8d8b08cc503a89e3d9eee7020ad9d2
turner <brunomn95@gmail.com>
2017-03-11 20:12:56 -0300

    first commit, gtk window sample

---
+++ src/GtkWindowSample.cpp
@@ -0,0 +1,35 @@
+//============================================================================
+// Name        : ine5420.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <gtk/gtk.h>
+
+static void
+activate (GtkApplication* app, gpointer user_data)
+{
+  GtkWidget *window;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 500, 500);
+  gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER_ALWAYS);
+  gtk_widget_show_all (window);
+}
+
+
+int main (int argc, char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
+}
